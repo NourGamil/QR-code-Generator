0e1bddee34bb739c8418b20d73aaed26384fa9c2
Nour Gamil <nourgamil135@gmail.com>
2025-08-06 11:40:13 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,90 @@
+"use client"
+// pages/index.js or a separate component
+
+import React, { useState,useRef } from 'react';
+    import { useQRCode } from 'next-qrcode';
+    // import html2pdf from 'html2pdf.js';
+    import html2canvas from 'html2canvas';
+
+    function QRCodeGenerator() {
+      const [qrValue, setQrValue] = useState('https://www.example.com');
+      const { Image } = useQRCode();
+
+      // const contentRef = useRef(null); // Create a ref to target the HTML content
+      // const generatePdf = () => {
+      //   const element = contentRef.current; // Get the HTML element
+      //   const options = {
+      //     margin:1,
+      //     filename: 'my-document.pdf',
+      //     image: { type: 'jpeg', quality: 0.98 },
+      //     html2canvas: { scale: 1, scrollX: 0, scrollY: 0, width: 320},
+        
+      //     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+      //   };
+      //   html2pdf().set(options).from(element).save();
+      // };
+
+      const handleExport = async () => {
+  const elementToCapture = document.getElementById('my-element-id'); // Target the element to convert
+  if (elementToCapture) {
+    const canvas = await html2canvas(elementToCapture);
+    const image = canvas.toDataURL('image/png'); // Get data URL of the PNG
+    
+    // Create a link to download the image
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'my-exported-image.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+};
+      return (
+        <div className='main'>
+          <div className='container'>
+            {/* ref={contentRef} */}
+            <div  id='my-element-id'>
+            <Image
+              text={qrValue === "" ||qrValue === null || qrValue === undefined ? ' ' : qrValue}
+              options={{
+                type: 'image/jpeg',
+                quality: 1,
+                errorCorrectionLevel: 'M',
+                margin: 3,
+                scale: 4,
+                width: 320,
+                color: {
+                  dark: '#000',
+                  light: '#FFFFFF',
+                },
+              }}
+            />
+          </div>
+          <div className='addsContainer'>
+            
+          <h1 className='qrtitle'>QR Code</h1>
+
+          <div className='inputContainer'>
+                      <label htmlFor="qrcodeInput" className='pointer'>URL</label>
+          <input
+            required
+            className='pointer mainInput'
+            type="text"
+            id='qrcodeInput'
+            value={qrValue}
+            onChange={(e) => setQrValue(e.target.value)}
+            placeholder="Enter text or URL"
+          />
+          </div>
+
+          <div className='btnsContainer'>
+          {/* <button onClick={generatePdf} className='pointer'>Generate PDF</button> */}
+          <button onClick={handleExport} className='pointer'>Generate PNG</button>
+          </div>
+
+          </div>
+          </div>
+        </div>
+      );
+    }
+export default QRCodeGenerator;
